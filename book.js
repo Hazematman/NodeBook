32d76b7e137f105df5ff43ba8250fe45955d5fbb
Lucas Fryzek <nercos7@gmail.com>
2019-02-05 13:22:39 -0500

    Add node dragging

--- book.js
+++ book.js
@@ -17,15 +17,61 @@ function resize_node(node, width, height)
     node.body.style.height = height.toString() + "px";
 }
 
+function move_node(node, x, y)
+{
+    node.left = x;
+    node.top = y;
+
+    node.container.style.top = y.toString() + "px";
+    node.container.style.left = x.toString() + "px";
+}
+
 function create_node()
 {
-    var node = {dragging:false, width:200, height:200};
+    var node = {dragging:false, move_dragging:false, width:200, height:200, top: 10, left: 10};
     node.container = document.createElement("div");
     node.container.style.position = "absolute";
     node.container.style.top = "10px";
     node.container.style.left = "10px";
     node.container.style.width = "200px";
     node.container.classList.add("node_container");
+
+    var on_move_drag = function(evt)
+    {
+        if(node.move_dragging)
+        {
+            var newX = evt.clientX;
+            var newY = evt.clientY;
+
+            var deltaX = newX - node.move_x;
+            var deltaY = newY - node.move_y;
+
+            node.move_x = newX;
+            node.move_y = newY;
+
+            move_node(node, node.left + deltaX, node.top + deltaY);
+        }
+    }
+
+    var stop_move_drag = function(evt)
+    {
+        node.move_dragging = false;
+
+        var view = document.getElementById("graph_view"); 
+        view.removeEventListener("mousemove", on_move_drag);
+        view.removeEventListener("mouseup", stop_move_drag);
+    }
+
+    var start_move_drag = function(evt)
+    {
+        node.move_dragging = true;
+        node.move_x = evt.clientX;
+        node.move_y = evt.clientY;
+
+        var view = document.getElementById("graph_view"); 
+        view.addEventListener("mousemove", on_move_drag);
+        view.addEventListener("mouseup", stop_move_drag);
+    }
 
     node.title = document.createElement("div");
     node.title.innerHTML = "Title";
@@ -34,6 +80,7 @@ function create_node()
     node.title.classList.add("node_title");
 
     node.title.addEventListener('keypress', title_edit);
+    node.title.addEventListener('mousedown', start_move_drag); 
 
     node.body = document.createElement("p");
     node.body.style.height = "200px";
@@ -42,7 +89,7 @@ function create_node()
     node.body.spellcheck = "true";
     node.body.classList.add("node_body");
 
-    var on_drag = function(evt)
+    var on_resize_drag = function(evt)
     {
         if(node.dragging)
         {
@@ -59,35 +106,30 @@ function create_node()
              
         }
     }
-
-    var stop_drag = function(evt)
+    var stop_resize_drag = function(evt)
     {
         node.dragging = false;
 
         var view = document.getElementById("graph_view"); 
-        view.removeEventListener("mousemove", on_drag);
-        view.removeEventListener("mouseup", stop_drag);
+        view.removeEventListener("mousemove", on_resize_drag);
+        view.removeEventListener("mouseup", stop_resize_drag);
     }
-
-    var start_drag = function(evt)
+    var start_resize_drag = function(evt)
     {
         node.dragging = true;
         node.x = evt.clientX;
         node.y = evt.clientY;
 
         var view = document.getElementById("graph_view"); 
-        view.addEventListener("mousemove", on_drag);
-        view.addEventListener("mouseup", stop_drag);
+        view.addEventListener("mousemove", on_resize_drag);
+        view.addEventListener("mouseup", stop_resize_drag);
     }
 
     node.resizer = document.createElement("div");
     node.resizer.classList.add("node_resizer");
     node.resizer.draggable = "false";
 
-    node.resizer.addEventListener("mousedown", start_drag);
-    //node.resizer.addEventListener("mousemove", on_drag);
-    //node.resizer.addEventListener("moveup", stop_drag);
-
+    node.resizer.addEventListener("mousedown", start_resize_drag);
     
     node.container.appendChild(node.title);
     node.container.appendChild(node.body);
