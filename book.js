d1de41b03929b383eda2bcfcffa3df845a7b3c68
Lucas Fryzek <nercos7@gmail.com>
2019-02-05 12:42:39 -0500

    Add drag to resize

--- book.js
+++ book.js
@@ -11,13 +11,15 @@ function title_edit(e)
 
 function resize_node(node, width, height)
 {
+    node.width = width;
+    node.height = height;
     node.container.style.width = width.toString() + "px";
-    node.body.height = height.toString() + "px";
+    node.body.style.height = height.toString() + "px";
 }
 
 function create_node()
 {
-    var node = {};
+    var node = {dragging:false, width:200, height:200};
     node.container = document.createElement("div");
     node.container.style.position = "absolute";
     node.container.style.top = "10px";
@@ -33,15 +35,58 @@ function create_node()
 
     node.title.addEventListener('keypress', title_edit);
 
-    node.body = document.createElement("div");
+    node.body = document.createElement("p");
     node.body.style.height = "200px";
     node.body.innerHTML = "Body";
     node.body.contentEditable = "true";
     node.body.spellcheck = "true";
     node.body.classList.add("node_body");
 
+    var on_drag = function(evt)
+    {
+        if(node.dragging)
+        {
+            var newX = evt.clientX;
+            var newY = evt.clientY;
+
+            var deltaX = newX - node.x;
+            var deltaY = newY - node.y;
+
+            node.x = newX;
+            node.y = newY;
+
+            resize_node(node, node.width + deltaX, node.height + deltaY);
+             
+        }
+    }
+
+    var stop_drag = function(evt)
+    {
+        node.dragging = false;
+
+        var view = document.getElementById("graph_view"); 
+        view.removeEventListener("mousemove", on_drag);
+        view.removeEventListener("mouseup", stop_drag);
+    }
+
+    var start_drag = function(evt)
+    {
+        node.dragging = true;
+        node.x = evt.clientX;
+        node.y = evt.clientY;
+
+        var view = document.getElementById("graph_view"); 
+        view.addEventListener("mousemove", on_drag);
+        view.addEventListener("mouseup", stop_drag);
+    }
+
     node.resizer = document.createElement("div");
     node.resizer.classList.add("node_resizer");
+    node.resizer.draggable = "false";
+
+    node.resizer.addEventListener("mousedown", start_drag);
+    //node.resizer.addEventListener("mousemove", on_drag);
+    //node.resizer.addEventListener("moveup", stop_drag);
 
     
     node.container.appendChild(node.title);
